d8ebc08c12b0201b17e1be1694c826d99d028609
Ladon Skliarov <ladonskliarov@gmail.com>
2024-08-22 18:00:21 +0300

    clean up

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 
 import '/presentation/home_screen/home_screen.dart';
 
-// Test data
-// flutter: [{id: 7d785c38-cd54-4a98-ab57-44e50ae646c1, field: [.X., .X., ...], start: {x: 2, y: 1}, end: {x: 0, y: 2}},
-// {id: 88746d24-bf68-4dea-a6b6-4a8fefb47eb9, field: [XXX., X..X, X..X, .XXX], start: {x: 0, y: 3}, end: {x: 3, y: 0}}]
-
 void main() {
  runApp(const MyApp());
 }
